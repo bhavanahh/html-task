6ef61c96831512b3e7dd0fc714ff52fa3f6e3043
Bhavana B <bhavanasivaganga88@gmail.com>
2025-06-01 16:36:42 +0530

    Add files via upload

---
+++ task32.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+// Question 1: Print "Hello, World!" to the console.
+
+
+function printHelloWorld() {
+  console.log("Hello, World!");
+}
+
+printHelloWorld(); 
+// Expected Output: Hello, World!
+
---
+++ task44.js
@@ -0,0 +1,21 @@
+[7:53 pm, 31/05/2025] Naàbi🤍🦋: // Question 13: Write two functions:
+// 1. convertObjectToString that takes an object and converts it to a JSON string and return.
+// 2. convertStringToObject that takes a JSON string and converts it back into an object and return.
+
+function convertObjectToString(obj) {
+  return JSON.stringify(obj);
+}
+
+function convertStringToObject(str) {
+  return JSON.parse(str);
+}
+
+const person = { name: "Alice", age: 30 };
+const personString = convertObjectToString(person); 
+console.log(personString); 
+// Expected Output: '{"name":"Alice","age":30}'
+
+const parsedPerson = convertStringToObject(personString);
+console.log(parsedPerson); 
+// Expected Output: { name: "Alice", age: 30 }
+
